7990878bf31f719dc7dd83496837b85909ef6e85
manojvishalkijo <118419691+manojvishalkijo@users.noreply.github.com>
2022-12-08 16:03:19 +0530

    Add files via upload

---
+++ 08-12-2022/test.js
@@ -0,0 +1,13 @@
+let name1="manoj";
+var name2="vishal";
+
+function newName(){
+   let name1="harsha";
+   var name2="varthini";
+ console.log(name1,name2); 
+ 
+
+}
+
+newName()
+console.log(name1,name2);
